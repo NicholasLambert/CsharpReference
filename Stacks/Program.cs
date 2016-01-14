41706655c9c5dd86b558d256da3a1b24aeb18ca5
nicholaslambert <nick@thedesigniac.com>
2016-01-13 23:56:11 -0700

    Created simple program demonstrating a Stack().
    Added README.txt
    Made changed to Program.cs

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * This program creates a stack object called myStack
+ * The stack is then given two new objects
+ * The objects are then printed back out, in the reverse order of what they were added
+ */
+using System;
+using System.Collections;
 
 namespace Stacks
 {
@@ -6,7 +12,26 @@ namespace Stacks
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			//Create Stack object
+			Stack myStack = new Stack();
+
+			//add some objects into my stack
+			myStack.Push("Hello");
+			myStack.Push ("World");
+
+			//Write some things to the console about the stack
+			Console.WriteLine("Stack Demo");
+			Console.WriteLine ("Stack Count: {0}", myStack.Count);
+
+			//function to print the stack by passing the stack into it
+			PrintStack (myStack);
+
+		}
+
+		public static void PrintStack(IEnumerable myCollection) {
+			foreach (Object obj in myCollection) {
+				Console.WriteLine(obj);
+			}
 		}
 	}
 }
---
+++ Stacks/README.txt
@@ -0,0 +1,3 @@
+﻿This is to create a reference for Stack usage in C#
+
+There is nothing special inside this project, however there will be good references created that can be used on later in more complex projects.
