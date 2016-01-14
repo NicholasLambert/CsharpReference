6ec0993fc45ef40accdbad670d817d5a63714489
nicholaslambert <nick@thedesigniac.com>
2016-01-14 00:20:42 -0700

    * Enum.csproj: Added simple enum program that shows how to create  and return information from an enum.
    * Program.cs:
    * AssemblyInfo.cs:

---
+++ Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This program demonstrates basic enum setup and usage
+ */
+
+using System;
+
+namespace Enum
+{
+	class MainClass
+	{
+		//declare enum and add values
+		enum Months { Jan, Feb, March, April, May, June, July, August };
+
+		public static void Main (string[] args)
+		{
+			//declare variables for demonstration
+			int x, y;
+
+			//assign variables to the value of each enum position
+			//have to cast the enum objects to an int
+			x = (int)Months.Jan;
+			y = (int)Months.May;
+
+			//prints the values 0 and 4
+			Console.WriteLine (x);
+			Console.WriteLine (y);
+		}
+	}
+}
