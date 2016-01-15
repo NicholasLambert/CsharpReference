7a886780a243ed0907e9a54fdd8b8fc14abcebd9
Nicholas Lambert <nick@thedesigniac.com>
2016-01-15 15:23:56 -0700

    Created project to demonstrate structs. More will be added over time.

---
+++ Structs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Structs
+{
+	//Struct Setup
+	public struct Person {
+		public string Name;
+		public int Age;
+		public Person(string name, int age) {
+			Name = name;
+			Age = age;
+		}
+	}
+
+	class MainClass
+	{
+		static void Main()
+		{
+			//Creates new person from the struct object
+			Person p1 = new Person ("Alex", 9);
+			Console.WriteLine ("p1 Name = {0} Age = {1}", p1.Name, p1.Age);
+		}
+	}
+}
